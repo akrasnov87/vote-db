6b982e9bc2ad818655f4b42eedbe6859086bdc59
Александр Краснов <a-krasnov@it-serv.ru>
2020-04-02 13:42:22 +0300

    обновление

--- SCHEMA/dbo/TABLE/cs_apartment.sql
+++ SCHEMA/dbo/TABLE/cs_apartment.sql
@@ -1,12 +1,11 @@
 CREATE TABLE dbo.cs_apartment (
 	id uuid DEFAULT public.uuid_generate_v4() NOT NULL,
 	f_house uuid,
-	c_prefix text,
 	c_number text,
 	n_number integer,
-	b_custom boolean NOT NULL,
 	dx_created timestamp with time zone DEFAULT now(),
-	sn_delete boolean NOT NULL DEFAULT false
+	sn_delete boolean DEFAULT false NOT NULL,
+	f_user integer
 );
 
 ALTER TABLE dbo.cs_apartment OWNER TO mobnius;
@@ -17,27 +16,28 @@ COMMENT ON COLUMN dbo.cs_apartment.id IS '[e60] Идентификатор';
 
 COMMENT ON COLUMN dbo.cs_apartment.f_house IS '[e50] Дом';
 
-COMMENT ON COLUMN dbo.cs_apartment.c_prefix IS '[e40] Префикс';
-
 COMMENT ON COLUMN dbo.cs_apartment.c_number IS '[e30] Номер';
 
 COMMENT ON COLUMN dbo.cs_apartment.n_number IS '[e20] Номер';
 
-COMMENT ON COLUMN dbo.cs_apartment.b_custom IS '[e10] Собственное';
-
---------------------------------------------------------------------------------
-
-ALTER TABLE dbo.cs_apartment
-	ADD CONSTRAINT cs_apartment_pkey PRIMARY KEY (id);
-	
 --------------------------------------------------------------------------------
 
 CREATE TRIGGER cs_apartment_trigger
 	BEFORE INSERT OR UPDATE OR DELETE ON dbo.cs_apartment
 	FOR EACH ROW
 	EXECUTE PROCEDURE core.cft_0_log_action();
-	
+
+--------------------------------------------------------------------------------
+
+ALTER TABLE dbo.cs_apartment
+	ADD CONSTRAINT cs_apartment_pkey PRIMARY KEY (id);
+
 --------------------------------------------------------------------------------
 
 ALTER TABLE dbo.cs_apartment
 	ADD CONSTRAINT cs_apartment_f_house_fkey FOREIGN KEY (f_house) REFERENCES dbo.cs_house(id);
+
+--------------------------------------------------------------------------------
+
+ALTER TABLE dbo.cs_apartment
+	ADD CONSTRAINT cs_apartment_f_user_fkey FOREIGN KEY (f_user) REFERENCES core.pd_users(id) NOT VALID;
--- SCHEMA/dbo/TABLE/cs_house.sql
+++ SCHEMA/dbo/TABLE/cs_house.sql
@@ -4,15 +4,18 @@ CREATE TABLE dbo.cs_house (
 	c_number text,
 	c_house_num text,
 	c_build_num text,
-	c_struc_num text,
+	c_height_num text,
 	n_number integer,
 	f_user integer,
 	n_longitude numeric(20,15) NOT NULL,
 	n_latitude numeric(20,15) NOT NULL,
 	c_data text,
-	b_student_hostel boolean NOT NULL,
 	dx_created timestamp with time zone DEFAULT now(),
-	sn_delete boolean NOT NULL DEFAULT false
+	sn_delete boolean DEFAULT false NOT NULL,
+	n_appartament integer,
+	b_mkd boolean NOT NULL,
+	b_private boolean NOT NULL,
+	b_self boolean NOT NULL
 );
 
 ALTER TABLE dbo.cs_house OWNER TO mobnius;
@@ -23,33 +26,39 @@ COMMENT ON COLUMN dbo.cs_house.id IS '[e70] Идентификатор';
 
 COMMENT ON COLUMN dbo.cs_house.f_street IS '[e60] Улица';
 
+COMMENT ON COLUMN dbo.cs_house.c_number IS 'Номер дома';
+
 COMMENT ON COLUMN dbo.cs_house.c_house_num IS '[e50] Номер дома';
 
 COMMENT ON COLUMN dbo.cs_house.c_build_num IS '[e40] Корпус';
 
-COMMENT ON COLUMN dbo.cs_house.c_struc_num IS '[e30] Строение';
+COMMENT ON COLUMN dbo.cs_house.c_height_num IS '[e30] Этажей';
 
 COMMENT ON COLUMN dbo.cs_house.c_data IS '[e20] Дома/квартиры через запятую';
 
-COMMENT ON COLUMN dbo.cs_house.b_student_hostel IS '[e10] Студенческое общежитие';
+COMMENT ON COLUMN dbo.cs_house.n_appartament IS 'Кол-во кв. в доме';
 
---------------------------------------------------------------------------------
+COMMENT ON COLUMN dbo.cs_house.b_mkd IS 'МКД';
 
-ALTER TABLE dbo.cs_house
-	ADD CONSTRAINT cs_house_pkey PRIMARY KEY (id);
-	
+COMMENT ON COLUMN dbo.cs_house.b_self IS 'Самострой';
+
 --------------------------------------------------------------------------------
 
 CREATE TRIGGER cs_house_trigger
 	BEFORE INSERT OR UPDATE OR DELETE ON dbo.cs_house
 	FOR EACH ROW
 	EXECUTE PROCEDURE core.cft_0_log_action();
-	
+
+--------------------------------------------------------------------------------
+
+ALTER TABLE dbo.cs_house
+	ADD CONSTRAINT cs_house_pkey PRIMARY KEY (id);
+
 --------------------------------------------------------------------------------
 
 ALTER TABLE dbo.cs_house
 	ADD CONSTRAINT cs_house_f_user_fkey FOREIGN KEY (f_user) REFERENCES core.pd_users(id);
-	
+
 --------------------------------------------------------------------------------
 
 ALTER TABLE dbo.cs_house
