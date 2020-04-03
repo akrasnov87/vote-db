56c53fa89b39f2f1f6b7bae8dbbc97980c7bc47b
Александр Краснов <akrasnov87@gmail.com>
2020-04-03 07:46:58 +0300

    обновление

---
+++ SCHEMA/dbo/FUNCTION/cf_bind_appartament.sql
@@ -0,0 +1,38 @@
+CREATE OR REPLACE FUNCTION dbo.cf_bind_appartament(_userid integer, _house uuid, _appartament_start integer, _appartamnt_finish integer) RETURNS integer
+    LANGUAGE plpgsql
+    AS $$
+DECLARE
+	_result integer;
+BEGIN
+	IF _house is not NULL AND _userid is not null THEN
+	
+		IF _appartament_start is null AND _appartamnt_finish is null THEN
+			update dbo.cs_appartament
+			set f_user = f_user
+			where f_house = _house;
+			
+			_result = 0;
+		ELSE
+			
+			IF _appartament_start is not null AND _appartamnt_finish is not null THEN
+				update dbo.cs_appartament
+				set f_user = f_user
+				where f_house = _house and n_number >= _appartament_start and n_number <= _appartamnt_finish;
+
+				_result = 0;
+			ELSE
+				_result = 2;
+			END IF;
+		
+		END IF;
+		
+	ELSE
+		_result = 1;
+	END IF;
+	RETURN _result;
+END
+$$;
+
+ALTER FUNCTION dbo.cf_bind_appartament(_userid integer, _house uuid, _appartament_start integer, _appartamnt_finish integer) OWNER TO mobnius;
+
+COMMENT ON FUNCTION dbo.cf_bind_appartament(_userid integer, _house uuid, _appartament_start integer, _appartamnt_finish integer) IS 'Привязка обходчиков к квартирам';
---
+++ SCHEMA/dbo/FUNCTION/cf_create_appartament.sql
@@ -0,0 +1,35 @@
+CREATE OR REPLACE FUNCTION dbo.cf_create_appartament(_house uuid, _appartament_start integer, _appartamnt_finish integer) RETURNS integer
+    LANGUAGE plpgsql
+    AS $$
+DECLARE
+	_result integer;
+BEGIN
+	IF _house is not NULL THEN
+	
+		IF _appartament_start is null AND _appartamnt_finish is null THEN
+			_result = 2;
+		ELSE
+			
+			IF _appartament_start is not null AND _appartamnt_finish is not null THEN
+			
+				insert into dbo.cs_appartament(f_house, c_number, n_number)
+				SELECT _house, s.a::text, s.a  FROM generate_series(_appartament_start,_appartamnt_finish) AS s(a);
+		
+				_result = 0;
+			ELSE
+				_result = 3;
+			END IF;
+		
+		END IF;
+		
+	ELSE
+		_result = 1;
+	END IF;
+	
+	RETURN _result;
+END
+$$;
+
+ALTER FUNCTION dbo.cf_create_appartament(_house uuid, _appartament_start integer, _appartamnt_finish integer) OWNER TO mobnius;
+
+COMMENT ON FUNCTION dbo.cf_create_appartament(_house uuid, _appartament_start integer, _appartamnt_finish integer) IS 'Создание квартир';
