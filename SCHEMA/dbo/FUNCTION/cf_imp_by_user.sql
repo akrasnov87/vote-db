151138e8d3b51f064840891fbfa5171a486049a3
Александр Краснов <akrasnov87@gmail.com>
2020-04-03 11:29:30 +0300

    импорт

---
+++ SCHEMA/dbo/FUNCTION/cf_imp_by_user.sql
@@ -0,0 +1,31 @@
+CREATE OR REPLACE FUNCTION dbo.cf_imp_by_user(_user_id integer, _route_type_id integer, _date_activate date) RETURNS integer
+    LANGUAGE plpgsql
+    AS $$
+DECLARE
+	_result integer;
+	_route_id uuid;
+	_route_number text;
+BEGIN
+	_route_id = uuid_generate_v4();
+	_route_number = concat('M', _route_type_id, '-', _user_id, '-', date_part('month', now()), date_part('day', now()));
+	-- создать маршрут
+	insert into core.cd_routes(id, f_type, c_number, d_date, d_date_start, d_date_end, b_extended, c_notice, n_order)
+	values (_route_id, _route_type_id, _route_number, now(), _date_activate, '2020-12-31', false, 'Создан автоматически', -1);
+	
+	-- привязать маршрут к пользователю
+	insert into core.cd_userinroutes(f_route, f_user, b_main)
+	values(_route_id, _user_id, true);
+	
+	-- добавить историю по маршруту
+	insert into core.cd_route_history (fn_route, fn_status, fn_user, d_date) values
+	(_route_id, 2, _user_id, now()),
+	(_route_id, 3, _user_id, now());
+	
+	_result = 0;
+	RETURN _result;
+END
+$$;
+
+ALTER FUNCTION dbo.cf_imp_by_user(_user_id integer, _route_type_id integer, _date_activate date) OWNER TO mobnius;
+
+COMMENT ON FUNCTION dbo.cf_imp_by_user(_user_id integer, _route_type_id integer, _date_activate date) IS 'Импорт данных для пользователя';
---
+++ SCHEMA/dbo/FUNCTION/cf_imp_registr_pts.sql
@@ -0,0 +1,33 @@
+CREATE OR REPLACE FUNCTION dbo.cf_imp_registr_pts() RETURNS integer
+    LANGUAGE plpgsql
+    AS $$
+DECLARE
+	_result integer;
+BEGIN
+	delete from dbo.ed_registr_pts
+	where f_subdivision is null;
+
+	insert into dbo.ed_registr_pts (id, c_subscr, c_device, c_address, n_longitude, n_latitude, f_user, f_division, b_disabled)
+	select 
+		a.id, 
+		a.c_number, 
+		a.n_number::text,
+		s.c_name,
+		h.n_longitude,
+		h.n_latitude,
+		a.f_user,
+		1,
+		false
+	from dbo.cs_apartament as a
+	inner join dbo.cs_house as h ON h.id = a.f_house
+	inner join dbo.cs_street as s ON h.f_street = s.id
+	where a.f_user is not null;
+	
+	_result = 0;
+	RETURN _result;
+END
+$$;
+
+ALTER FUNCTION dbo.cf_imp_registr_pts() OWNER TO mobnius;
+
+COMMENT ON FUNCTION dbo.cf_imp_registr_pts() IS 'Обновление точек учета по всем пользователям';
