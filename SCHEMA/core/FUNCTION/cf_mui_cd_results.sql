cc520fea2d525cb5a7ab093fc972ae2ffe4d3ecb
Александр Краснов <akrasnov87@gmail.com>
2020-04-13 16:06:57 +0300

    обновление

--- SCHEMA/core/FUNCTION/cf_mui_cd_results.sql
+++ SCHEMA/core/FUNCTION/cf_mui_cd_results.sql
@@ -1,9 +1,9 @@
-CREATE OR REPLACE FUNCTION core.cf_mui_cd_results(_fn_user integer) RETURNS TABLE(id uuid, fn_route uuid, fn_point uuid, fn_user_point uuid, fn_type integer, fn_user integer, d_date timestamp with time zone, c_notice text, b_warning boolean, jb_data text, n_order integer, dx_created timestamp with time zone)
+CREATE OR REPLACE FUNCTION core.cf_mui_cd_results(_fn_user integer) RETURNS TABLE(id uuid, fn_route uuid, fn_point uuid, fn_user_point uuid, fn_type integer, fn_user integer, fn_question integer, fn_answer integer, d_date timestamp with time zone, c_notice text, b_warning boolean, jb_data text, n_order integer, dx_created timestamp with time zone)
     LANGUAGE plpgsql STABLE
     AS $$
 
 BEGIN
-	RETURN QUERY select r.id, r.fn_route, r.fn_point, r.fn_user_point, r.fn_type, r.fn_user, r.d_date, r.c_notice, r.b_warning, r.jb_data::text, r.n_order, r.dx_created 
+	RETURN QUERY select r.id, r.fn_route, r.fn_point, r.fn_user_point, r.fn_type, r.fn_user, r.fn_question, r.fn_answer, r.d_date, r.c_notice, r.b_warning, r.jb_data::text, r.n_order, r.dx_created 
     from core.cd_userinroutes as uir
     LEFT JOIN core.cd_routes as rt ON rt.id = uir.f_route
     INNER JOIN core.cd_results as r ON r.fn_route = rt.id
