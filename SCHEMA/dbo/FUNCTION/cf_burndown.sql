7f384f26344a21a81e03cbf91316d5440c94a0ba
Александр Краснов <akrasnov87@gmail.com>
2020-05-29 08:31:56 +0300

    обновление

--- SCHEMA/dbo/FUNCTION/cf_burndown.sql
+++ SCHEMA/dbo/FUNCTION/cf_burndown.sql
@@ -3,28 +3,14 @@ CREATE OR REPLACE FUNCTION dbo.cf_burndown(_type integer, _userid integer) RETUR
     AS $$
 
 BEGIN
-	RETURN QUERY 
-	select 10, '2020-05-01'
-	UNION ALL
-	select 11, '2020-05-03'
-	UNION ALL
-	select 20, '2020-05-05'
-	UNION ALL
-	select 15, '2020-05-10'
-	UNION ALL
-	select 21, '2020-05-12'
-	UNION ALL
-	select 11, '2020-05-15'
-	UNION ALL
-	select 16, '2020-05-16';
-    --RETURN QUERY select
-	--	count(*) as n_count,
-	--	(SELECT date_trunc('day', up.d_date))::date as d_date
-	--from core.cd_user_points as up
-	--inner join core.cd_routes as r ON up.fn_route = r.id
-	--where up.fn_type = _type and up.fn_user = _userId
-	--group by (SELECT date_trunc('day', up.d_date))
-	--order by (SELECT date_trunc('day', up.d_date)) asc;
+    RETURN QUERY select
+		count(*) as n_count,
+		(SELECT date_trunc('day', up.d_date))::date as d_date
+	from core.cd_user_points as up
+	inner join core.cd_routes as r ON up.fn_route = r.id
+	where up.fn_type = _type and up.fn_user = _userId
+	group by (SELECT date_trunc('day', up.d_date))
+	order by (SELECT date_trunc('day', up.d_date)) asc;
 END
 $$;
 
