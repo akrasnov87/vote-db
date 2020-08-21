e8ed3126049527065613f0fd285e15e30ed1b662
Aleksandr Krasnov <akrasnov87@gmail.com>
2020-08-21 13:48:05 +0300

    update

--- SCHEMA/core/FUNCTION/cft_cd_results_trigger.sql
+++ SCHEMA/core/FUNCTION/cft_cd_results_trigger.sql
@@ -5,7 +5,7 @@ BEGIN
     IF (TG_OP = 'INSERT') THEN
 		UPDATE core.cd_results
 		set b_disabled = true
-		where fn_point = NEW.fn_point and id != NEW.id;
+		where fn_route = NEW.fn_route and fn_point = NEW.fn_point and id != NEW.id;
       RETURN NEW;
     END IF;
 
