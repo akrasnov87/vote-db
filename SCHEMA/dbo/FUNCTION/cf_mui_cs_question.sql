22b2c775a938fc2e5318a0d9b7fcfee9669aae71
Aleksandr Krasnov <akrasnov87@gmail.com>
2020-09-01 14:11:25 +0300

    обновление

--- SCHEMA/dbo/FUNCTION/cf_mui_cs_question.sql
+++ SCHEMA/dbo/FUNCTION/cf_mui_cs_question.sql
@@ -1,22 +1,9 @@
 CREATE OR REPLACE FUNCTION dbo.cf_mui_cs_question(_fn_user integer) RETURNS TABLE(id bigint, c_title text, c_description text, c_text text, n_order integer, dx_created timestamp with time zone, sn_delete boolean, c_role text, n_priority integer)
     LANGUAGE plpgsql STABLE
     AS $$
-DECLARE
-	_n_code integer;
-	_c_fio text;
-	_c_status text;
 BEGIN
-	select sd.n_code into _n_code from core.pd_users as u
-	inner join core.sd_subdivisions as sd ON sd.id = u.f_subdivision
-	where u.id = _fn_user;
-	
-	select c_fio into _c_fio from dbo.cs_main_candidate
-	where n_subdivision = _n_code;
-	
-	select c_status into _c_status from dbo.cs_main_candidate
-	where n_subdivision = _n_code;
 
-    RETURN QUERY select q.id, q.c_title, q.c_description, REPLACE(REPLACE(q.c_text, '[c_candidate_name]', _c_fio), '[c_candidate_status]', _c_status), q.n_order, q.dx_created, q.sn_delete, r.c_name, q.n_priority 
+    RETURN QUERY select q.id, q.c_title, q.c_description, q.c_text, q.n_order, q.dx_created, q.sn_delete, r.c_name, q.n_priority 
 	from dbo.cs_question as q
 	left join core.pd_roles as r ON q.f_role = r.id
 	where q.b_disabled = false;
--- SCHEMA/dbo/FUNCTION/cf_statistic.sql
+++ SCHEMA/dbo/FUNCTION/cf_statistic.sql
@@ -6,15 +6,46 @@ DECLARE
 	_c_fio text;
 	_n_count integer;
 	_n_result integer;
+	_not_count integer;
+	_not_open_count integer;
+	_open1_count integer;
+	_open2_count integer;
+	_other_count integer;
 BEGIN
 	select coalesce(u.c_fio, u.c_login) into _c_fio from core.pd_users as u
 	where u.id = _f_user;
 	select cf_mui_cd_points_count into _n_count from core.cf_mui_cd_points_count(_f_user, 0);
+	
 	select cf_mui_cd_results_count into _n_result from core.cf_mui_cd_results_count(_f_user);
 	
+	select count(*) into _not_count from core.cf_mui_cd_results(_f_user) as r
+	inner join dbo.cs_answer as a ON r.fn_answer = a.id
+	where a.c_color = '#000000';
+	
+	select count(*) into _not_open_count from core.cf_mui_cd_results(_f_user) as r
+	inner join dbo.cs_answer as a ON r.fn_answer = a.id
+	where a.c_color = '#8b0000';
+	
+	select count(*) into _open1_count from core.cf_mui_cd_results(_f_user) as r
+	inner join dbo.cs_answer as a ON r.fn_answer = a.id
+	where a.c_color = '#00008b,#0000ff,#00ffff';
+	
+	select count(*) into _open2_count from core.cf_mui_cd_results(_f_user) as r
+	inner join dbo.cs_answer as a ON r.fn_answer = a.id
+	where a.c_color = '#808000,#006400,#00ff00';
+	
+	select count(*) into _other_count from core.cf_mui_cd_results(_f_user) as r
+	inner join dbo.cs_answer as a ON r.fn_answer = a.id
+	where a.c_color = '#F57F17';
+	
 	_result = concat('<p><b>«', _c_fio,'»</b>, доброго времени суток!<p>');
-	_result = concat(_result, '<p>Кол-во квартир – ', _n_count::text, '(100%)<br />');
-	_result = concat(_result, ' - кол-во пройденных квартир – ', _n_result, '(', (_n_result * 100 / _n_count), '%)');
+	_result = concat(_result, '<p>Кол-во квартир – ', _n_count::text, ' (100%)<br />');
+	_result = concat(_result, '<br /> - кол-во пройденных квартир – ', _n_result, ' (', (_n_result * 100 / _n_count), '%)');
+	_result = concat(_result, '<br /> - никого нет дома – ', _not_count, ' (', (_not_count * 100 / CASE WHEN _n_result = 0 THEN 1 ELSE _n_result END), '%)');
+	_result = concat(_result, '<br /> - отказ открывать дверь – ', _not_open_count, ' (', (_not_open_count * 100 / CASE WHEN _n_result = 0 THEN 1 ELSE _n_result END), '%)');
+	_result = concat(_result, '<br /> - открыли – АПМ НЕ вручен в руки – ', _open1_count, ' (', (_open1_count * 100 / CASE WHEN _n_result = 0 THEN 1 ELSE _n_result END), '%)');
+	_result = concat(_result, '<br /> - открыли – АПМ вручен в руки – ', _open2_count, ' (', (_open2_count * 100 / CASE WHEN _n_result = 0 THEN 1 ELSE _n_result END), '%)');
+	_result = concat(_result, '<br /> - другое – ', _other_count, ' (', (_other_count * 100 / CASE WHEN _n_result = 0 THEN 1 ELSE _n_result END), '%)</p>');
 	RETURN _result;
 END
 $$;
